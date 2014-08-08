4c1ac4cb6e317a2388589a9fb7e9c3c0d2bb844c
Max Kovpak <max_kovpak@hotmail.com>
2014-08-09 02:21:12 +0300

    fix isset

--- src/views/files_upload_settings.blade.php
+++ src/views/files_upload_settings.blade.php
@@ -1,19 +1,19 @@
 @if (isset($field_info['model']))
     <?php $model_for_files_upload = $field_info['model']; ?>
 @else
-    <?php $model_for_files_upload = $module['model']; ?>
+    <?php $model_for_files_upload = isset($module['model']) ? $module['model'] : ''; ?>
 @endif
 
 @if (isset($field_info['module']))
     <?php $module_for_files_upload = $field_info['module']; ?>
 @else
-    <?php $module_for_files_upload = $module['name']; ?>
+    <?php $module_for_files_upload = isset($module['name']) ? $module['name'] : ''; ?>
 @endif
 
 @if (isset($field_info['field_name']))
     <?php $name_for_files_upload = $field_info['field_name']; ?>
 @else
-    <?php $name_for_files_upload = $field_input_name; ?>
+    <?php $name_for_files_upload = isset($field_input_name) ? $field_input_name : ''; ?>
 @endif
 
 @if (isset($field_info['action']))
